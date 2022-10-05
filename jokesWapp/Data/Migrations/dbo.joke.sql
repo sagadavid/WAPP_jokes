7bd756789808e6c19f5a49db1fb6fa2ed24970dc
David Saga <davidsa@uia.no>
2022-10-05 09:09:51 +0200

    migration file

---
+++ jokesWapp/Data/Migrations/dbo.joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[joke] (
+    [id]       INT            IDENTITY (1, 1) NOT NULL,
+    [question] NVARCHAR (MAX) NULL,
+    [answer]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_joke] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
